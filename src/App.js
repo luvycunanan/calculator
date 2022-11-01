f25db987c6692af1b74cc23d27120ed9fdebea6b
luvycunanan <luvy.cun@gmail.com>
2022-10-31 19:33:00 -0700

    basic calculator structure done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <div className="main">
+        <div className="display">display</div>
+        <div className="button-container">
+          <div className="btn-row">
+            <button className="btn-col othr">AC</button>
+            <button className="btn-col othr">+/-</button>
+            <button className="btn-col othr">%</button>
+            <button className="btn-col oprt">/</button>
+          </div>
+          <div className="btn-row">
+            <button className="btn-col num">7</button>
+            <button className="btn-col num">8</button>
+            <button className="btn-col num">9</button>
+            <button className="btn-col num oprt">x</button>
+          </div>
+          <div className="btn-row">
+            <button className="btn-col num">4</button>
+            <button className="btn-col num">5</button>
+            <button className="btn-col num">6</button>
+            <button className="btn-col oprt">-</button>
+          </div>
+          <div className="btn-row">
+            <button className="btn-col num">1</button>
+            <button className="btn-col num">2</button>
+            <button className="btn-col num">3</button>
+            <button className="btn-col oprt">+</button>
+          </div>
+          <div className="btn-row">
+            <button className="btn-col num zero">0</button>
+            <button className="btn-col num">.</button>
+            <button className="btn-col oprt">=</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
